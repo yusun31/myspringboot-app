6d00dd241232f9316e1cb72b563795df256656ab
yusun <yousun2286@naver.com>
2022-04-19 15:36:12 +0900

    DBRunner

---
+++ src/main/java/com/example/myspringboot/runner/DBRunner.java
@@ -0,0 +1,26 @@
+package com.example.myspringboot.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+
+
+@Component
+public class DBRunner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("DataSource 구현클래스 이름: " + dataSource.getClass().getName());
+        // ctrl + Alt + v: return type 자동생성
+        Connection connection = dataSource.getConnection();
+        DatabaseMetaData metaData = connection.getMetaData();
+    }
+}
