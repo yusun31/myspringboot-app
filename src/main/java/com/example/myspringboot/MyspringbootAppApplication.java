ac94182f0954fc203d800bf29958acdf5f8b2e61
yusun <yousun2286@naver.com>
2022-04-19 14:53:03 +0900

    Initial commit

---
+++ src/main/java/com/example/myspringboot/MyspringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringbootAppApplication.class, args);
+	}
+
+}
