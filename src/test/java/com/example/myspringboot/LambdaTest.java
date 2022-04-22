690ef1d483bfcae09890747ed50ffa2a8517ab84
yusun <yousun2286@naver.com>
2022-04-22 16:35:17 +0900

    lambda

--- src/test/java/com/example/myspringboot/LambdaTest.java
+++ src/test/java/com/example/myspringboot/LambdaTest.java
@@ -1,5 +1,8 @@
 package com.example.myspringboot;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.jupiter.api.Test;
 
 public class LambdaTest {
@@ -17,7 +20,24 @@ public class LambdaTest {
             }
         });
         t2.start();
+
+        // 3. Lambda Expression
+        // Runnable이 가진 run() 메소드를 재정의하는 구문을 람다식으로 표현할 수 있음
+        Thread t3 = new Thread(() -> System.out.println("람다식으로 구현함"));
+        t3.start();
     }
+}
+
+@Getter @Setter
+@AllArgsConstructor // 생성자 쓰지 않아도 자동으로 만들어줌
+class User {
+    private String name;
+    private int age;
+
+//    public User(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
 }
 
 class MyRunnable implements Runnable {
