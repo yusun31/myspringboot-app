775c9fad9166421dac01077d394c4b6731c0ab13
yusun <yousun2286@naver.com>
2022-04-26 13:28:16 +0900

    docker build

--- src/test/java/com/example/myspringboot/MyspringbootAppApplicationTests.java
+++ src/test/java/com/example/myspringboot/MyspringbootAppApplicationTests.java
@@ -1,9 +1,7 @@
 package com.example.myspringboot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MyspringbootAppApplicationTests {
 
 	@Test
--- src/test/java/com/example/myspringboot/repository/AccountRepositoryTest.java
+++ src/test/java/com/example/myspringboot/repository/AccountRepositoryTest.java
@@ -1,50 +1,44 @@
 package com.example.myspringboot.repository;
 
-import com.example.myspringboot.entity.Account;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
 
 @SpringBootTest
 public class AccountRepositoryTest {
-    @Autowired
-    AccountRepository accountRepository;
-
-    @Test
-    @Disabled
-    public void crud() throws Exception {
-        // 1. insert
-        // Account 객체 생성
-        Account account = new Account();
-        // 값 저장
-        account.setUsername("spring");
-        account.setPassword("1234");
-        //등록요청
-        Account savedAccount = accountRepository.save(account);
-        System.out.println("ID: " + savedAccount.getId());
-        System.out.println("username: " + savedAccount.getUsername());
-    }
-
-    @Test
-    public void finder() throws Exception {
-        // ctrl + alt + v
-        Optional<Account> optional = accountRepository.findById(1L);
-        if(optional.isPresent()) {
-            Account account = optional.get();
-            System.out.println("Account " + account.getUsername());
-        }
-
-        Account acct = accountRepository.findById(10L).orElse(new Account());
-        System.out.println("Account " + acct.getUsername());
-
-        // OrElseGet(Supplier sup)
-        // Supplier의 추상메서드 T get()
-        Account acct2 = accountRepository.findById(2L).orElseGet(() -> new Account(""));
-        System.out.println("acct2.getUsername() = " + acct2.getUsername());
+//    @Autowired
+//    AccountRepository accountRepository;
+//
+//    @Test
+//    @Disabled
+//    public void crud() throws Exception {
+//        // 1. insert
+//        // Account 객체 생성
+//        Account account = new Account();
+//        // 값 저장
+//        account.setUsername("spring");
+//        account.setPassword("1234");
+//        //등록요청
+//        Account savedAccount = accountRepository.save(account);
+//        System.out.println("ID: " + savedAccount.getId());
+//        System.out.println("username: " + savedAccount.getUsername());
+//    }
+//
+//    @Test
+//    public void finder() throws Exception {
+//        // ctrl + alt + v
+//        Optional<Account> optional = accountRepository.findById(1L);
+//        if(optional.isPresent()) {
+//            Account account = optional.get();
+//            System.out.println("Account " + account.getUsername());
+//        }
+//
+//        Account acct = accountRepository.findById(10L).orElse(new Account());
+//        System.out.println("Account " + acct.getUsername());
+//
+//        // OrElseGet(Supplier sup)
+//        // Supplier의 추상메서드 T get()
+//        Account acct2 = accountRepository.findById(2L).orElseGet(() -> new Account(""));
+//        System.out.println("acct2.getUsername() = " + acct2.getUsername());
 
         //Account acct3 = accountRepository.findById(3L).orElseThrow(() -> new RuntimeException("Account Not Found"));
-    }
+//    }
 }
