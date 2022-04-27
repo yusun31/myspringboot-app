c28ff146c02f0a10747349dbf5c8e85e90d2f7c6
yusun <yousun2286@naver.com>
2022-04-27 15:26:14 +0900

    edit form

--- src/main/java/com/example/myspringboot/controller/UserController.java
+++ src/main/java/com/example/myspringboot/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -51,6 +52,16 @@ public class UserController {
         userService.insertUser(user);
         model.addAttribute("users", userService.selectAllUser());
         return "index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        User user = userService.selectUser(id);
+        UserForm userForm = new UserForm();
+        BeanUtils.copyProperties(user, userForm);
+        //User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("userForm", userForm);
+        return "update-user";
     }
 
 }
