0cc26430f492a14bbfda921eb89911c76f4b1b33
yusun <yousun2286@naver.com>
2022-04-27 14:46:21 +0900

    Form update

--- src/main/java/com/example/myspringboot/controller/form/UserForm.java
+++ src/main/java/com/example/myspringboot/controller/form/UserForm.java
@@ -5,15 +5,14 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 public class UserForm {
-    @NotEmpty(message = "이름은 필수 입력항목입니다.")
+    @NotBlank(message = "이름은 필수 입력항목입니다.")
     private String name;
 
-    @NotBlank(message = "이메일은 필수 입력항목입니다.")
-    @Email(message = "올바른 이메일 형식이 압니다.")
+    //@NotBlank(message = "이메일은 필수 입력항목입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
 }
