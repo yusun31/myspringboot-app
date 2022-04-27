ffb67f7814809fa3754b846bac39946de85373b8
yusun <yousun2286@naver.com>
2022-04-27 11:48:36 +0900

    ㅕãvalidation

--- src/main/java/com/example/myspringboot/entity/User.java
+++ src/main/java/com/example/myspringboot/entity/User.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Getter @Setter
@@ -12,9 +14,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "이름은 필수 입력항목입니다.")
     @Column
     private String name;
 
+    @NotBlank(message = "이메일은 필수 입력항목입니다.")
     @Column(unique=true)
     private String email;
 }
